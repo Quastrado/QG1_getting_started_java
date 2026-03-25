7c9fd711bf3fe1bf76a3d740e8caf30ce4aa316e
Quastrado <Ataxyswriter@yandex.ru>
2026-03-25 20:29:11 +0300

    hello!

--- src/test/java/SerchTests.java
+++ src/test/java/SerchTests.java
@@ -8,6 +8,7 @@ public class SearchTests {
     @Test
     void successfulSearchTest() {
         open("https://www.github.com/");
+        System.out.printLn("Hello!");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
     }
